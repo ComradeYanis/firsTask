b5832f8fc60282e6aac7c596c6502b9ec006976c
ComradeYanis <31801470+ComradeYanis@users.noreply.github.com>
2017-11-03 11:31:19 +0300

    Update my_sqrt.js
    changed Math.pow to formula

--- my_sqrt.js
+++ my_sqrt.js
@@ -4,11 +4,26 @@ class my_sqrt{
 		this.number_after_the_comma = number_after_the_comma;
 	}
 
+	//Формула Ньютона
+	formula(number, EPS){
+		var s = number;
+		var b = number;
+		var a = 1;
+		while(Math.abs(a-b) > EPS){
+			a = (a+b)/2;
+			b = s / a;
+		}
+		return ((a+b)/2);
+	}
+
 	complete(){
 		var a = Math.pow(10, this.number_after_the_comma);
-		return(parseFloat(Math.round(Math.pow(this.number,0.5) * a)/a).toFixed(this.number_after_the_comma));
+		var ready = formula(this.number, 1/a);
+		return(parseFloat(Math.round(ready * a)/a));
 	}
-}
+
+	
+} 
 
 function DoMath(){
 	var num1 = document.getElementById('num1').value;
